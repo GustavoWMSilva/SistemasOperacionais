8438f082090c93c2a2824a4d4af0f79bac901455
Gustavo Silva <110947375+GustavoWMSilva@users.noreply.github.com>
2024-04-07 13:09:24 -0300

    Fila carnivoros.c

--- carnivoros.c
+++ carnivoros.c
@@ -3,51 +3,43 @@
 #include <pthread.h>
 #include <semaphore.h>
 #include <unistd.h>
-#include <stdbool.h>
 
-typedef struct {
-    int M;
-    int N;
-    int indice;
-} ParametrosCanibal;
+#define MAX_CANIBAIS 5
+#define M 10
 
-bool *interesse;
-int vez = 0;
 
 int pratos_sujos = 0;
 sem_t mutex, cheia, cozinhando;
+int fila[MAX_CANIBAIS];
+int inicio = 0, fim = 0;
 
 void *canibais(void *arg)
 {
-    ParametrosCanibal *params = (ParametrosCanibal *)arg;
-    int M = params->M;
-    int N = params -> N;
-    long int id = params->indice;
+    long id = (long)arg;
 
     while (1) {
-        interesse[id] = true;
-        vez = (id + 1) % N;
-        while (interesse[vez] && vez != id)
-            ;
-        
+        sem_wait(&mutex);
+        fila[fim] = id;
+        fim = (fim + 1) % MAX_CANIBAIS;
+        printf("\nCanibal %ld chegou. Pratos sujos: %d", id, pratos_sujos);
+        sem_post(&mutex);
 
         sem_wait(&cozinhando);
         sem_wait(&mutex);
         printf("\nCanibal %ld comeu. Pratos sujos: %d", id, pratos_sujos);
+        
         pratos_sujos++;
         if (pratos_sujos == M)
             sem_post(&cheia); // Avisa o cozinheiro que todos os pratos estao sujos
         sem_post(&mutex);
         sem_post(&cozinhando);
-
-        interesse[id] = false;
         sleep(1);
     }
 }
 
 void *cozinheiro(void *arg)
 {
-    int M = *((int *)arg);
+    //int M = *((int *)arg);
 
     while (1) {
         sem_wait(&cheia); // Espera a travessa estar vazia
@@ -63,31 +55,20 @@ void *cozinheiro(void *arg)
 int main()
 {
     int N = 5;
-    int M = 10;
+    //int M = 10;
 
     pthread_t tcozinheiro, tcanibais[N];
 
     sem_init(&mutex, 0, 1);
     sem_init(&cheia, 0, 0);
-    sem_init(&fila_espera, 0, N);
     sem_init(&cozinhando, 0, M);
 
-    interesse = (bool *)malloc(N * sizeof(bool));
-    if (interesse == NULL) {
-        fprintf(stderr, "Erro ao alocar memória para interesse.\n");
-        exit(EXIT_FAILURE);
-    }
-
-    ParametrosCanibal params_canibal;
-    params_canibal.M = M;
-    params_canibal.N = N;
-
-    pthread_create(&tcozinheiro, NULL, cozinheiro, (void *)&M);
     for (long int i = 0; i < N; i++) {
-        interesse[i] = false;
-        params_canibal.indice = i;
-        pthread_create(&tcanibais[i], NULL, canibais, (void *)&params_canibal);
+        pthread_create(&tcanibais[i], NULL, canibais, (void *)i);
     }
+
+    //pthread_create(&tcozinheiro, NULL, cozinheiro, (void *)&M);
+    pthread_create(&tcozinheiro, NULL, cozinheiro, (void *)0);
 
     pthread_exit(NULL);
 
