ca5a23103392402480b9b8740c710a7c70f7b1bf
Gustavo Silva <110947375+GustavoWMSilva@users.noreply.github.com>
2024-04-07 18:24:19 -0300

    Entendi carnivoros.c

--- carnivoros.c
+++ carnivoros.c
@@ -4,73 +4,66 @@
 #include <semaphore.h>
 #include <unistd.h>
 
-#define MAX_CANIBAIS 5
+#define N 5
 #define M 10
 
+int vez;
+int flag[N];
+int porcoes_na_travessa = 0;
 
-int pratos_sujos = 0;
-sem_t mutex, cheia, cozinhando;
-int fila[MAX_CANIBAIS];
-int inicio = 0, fim = 0;
-
-void *canibais(void *arg)
-{
-    long id = (long)arg;
-
-    while (1) {
-        sem_wait(&mutex);
-        fila[fim] = id;
-        fim = (fim + 1) % MAX_CANIBAIS;
-        printf("\nCanibal %ld chegou. Pratos sujos: %d", id, pratos_sujos);
-        sem_post(&mutex);
-
-        sem_wait(&cozinhando);
-        sem_wait(&mutex);
-        printf("\nCanibal %ld comeu. Pratos sujos: %d", id, pratos_sujos);
-        
-        pratos_sujos++;
-        if (pratos_sujos == M)
-            sem_post(&cheia); // Avisa o cozinheiro que todos os pratos estao sujos
-        sem_post(&mutex);
-        sem_post(&cozinhando);
-        sleep(1);
+void lock(int id) {
+    int outro;
+    flag[id] = 1;
+    vez = id;
+    for (outro = 0; outro < N; outro++) {
+        if (outro != id) {
+            while (flag[outro] && vez == id);
+        }
     }
 }
 
-void *cozinheiro(void *arg)
-{
-    //int M = *((int *)arg);
+void unlock(int id) {
+    flag[id] = 0;
+}
 
+void *cozinheiro(void *arg) {
     while (1) {
-        sem_wait(&cheia); // Espera a travessa estar vazia
-        sem_wait(&mutex);
-        printf("\nCozinheiro está preparando comida...");
-        pratos_sujos = 0;
-        sem_post(&mutex);
-        for (int i = 0; i < M; i++)
-            sem_post(&cozinhando); // Enche a travessa
+        if (porcoes_na_travessa == 0) {
+            printf("Cozinheiro repondo a travessa...\n");
+            porcoes_na_travessa = M;
+        }
     }
 }
 
-int main()
-{
-    int N = 5;
-    //int M = 10;
+void *canibal(void *arg) {
+    int id = *((int *)arg);
+    while (1) {
+        lock(id);
+        if (porcoes_na_travessa > 0) {
+            porcoes_na_travessa--;
+            printf("Canibal %d se serviu e comeu. Porções restantes: %d\n", id, porcoes_na_travessa);
+        }
+        unlock(id);
+        usleep(500000); // Espera simulada para simular o tempo entre as ações dos canibais
+    }
+}
 
-    pthread_t tcozinheiro, tcanibais[N];
+int main() {
+    pthread_t threads_canibais[N], thread_cozinheiro;
+    int ids_canibais[N];
 
-    sem_init(&mutex, 0, 1);
-    sem_init(&cheia, 0, 0);
-    sem_init(&cozinhando, 0, M);
+    pthread_create(&thread_cozinheiro, NULL, cozinheiro, NULL);
 
-    for (long int i = 0; i < N; i++) {
-        pthread_create(&tcanibais[i], NULL, canibais, (void *)i);
+    for (int i = 0; i < N; i++) {
+        ids_canibais[i] = i;
+        pthread_create(&threads_canibais[i], NULL, canibal, &ids_canibais[i]);
     }
 
-    //pthread_create(&tcozinheiro, NULL, cozinheiro, (void *)&M);
-    pthread_create(&tcozinheiro, NULL, cozinheiro, (void *)0);
+    pthread_join(thread_cozinheiro, NULL);
 
-    pthread_exit(NULL);
+    for (int i = 0; i < N; i++) {
+        pthread_join(threads_canibais[i], NULL);
+    }
 
     return 0;
 }
